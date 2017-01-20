bb145914f104ba9963aeae5c3f9076e67031c4cf
elvinLong <970099734@qq.com>
2017-01-20 18:42:49 +0800

    no message

--- data/post-list.js
+++ data/post-list.js
@@ -13,7 +13,12 @@ module.exports = {
             "image": "http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg",
             "content": "目前，单位的标签来源主要来源于单位网络，如机构网络，企业网络，单位的标签显示为它们在网络分类里的最小子分类即最小子集，如 银行-城商行-江苏-苏州银行，则只显示江苏就可以，但这个江苏是包含在整个机构网络的。一个单位在单位网络里可以分在多个子集下，因此应该单位有多个标签",
             "zan": 99,
-            "like": 100
+            "like": 100,
+            "music": {
+                url: "http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46",
+                name: "许巍：此时此刻",
+                image: "http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000"
+            }
         },
         {
             "id": 2,
@@ -23,7 +28,12 @@ module.exports = {
             "image": "http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg",
             "content": "幸福大厦范德萨发地方萨芬，范德萨",
             "zan": 1,
-            "like": 20
+            "like": 20,
+            "music": {
+                url: "http://ws.stream.qqmusic.qq.com/C100002I8eGJ28BI17.m4a?fromtag=38",
+                name: "谭咏麟：朋友",
+                image: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004eGsCN3SUheO.jpg?max_age=2592000"
+            }
         }
     ]
 }
--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,24 +1,103 @@
-import {postList} from '../../../data/post-list'
+import { postList } from '../../../data/post-list'
 
 Page({
-  data:{},
-  onLoad:function(options){
+  data: {
+    musicStatus: false
+  },
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
     let { id } = options
-    let postObj = postList.filter((item) => (item.id == id))
+    var that = this
+    let result = postList.filter((item) => (item.id == id))
+    let postData = result ? result[0] : {}
+    let collectedList = wx.getStorageSync('collectedList')
 
-    this.setData({postData: postObj ? postObj[0] : {}})    
+    if (collectedList) {
+      postData.collected = collectedList[id]
+    } else {
+      wx.setStorageSync('collectedList', { [id]: false })
+    }
+    this.setData({ postData, id })
+
+    wx.onBackgroundAudioPlay(function() {
+      that.setData({
+        musicStatus: true
+      })
+       console.log(that.musicStatus)
+    })
+    wx.onBackgroundAudioStop(function() {
+      that.setData({
+        musicStatus: false
+      })
+      console.log(that.musicStatus)
+    })
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
+  },
+  onCollectionTap: function (event) {
+    let {id, postData} = this.data
+    let collectedList = wx.getStorageSync('collectedList') || {}
+    let collected = !collectedList[this.data.id]
+
+    collectedList[this.data.id] = collected
+    postData.collected = collected
+    wx.setStorageSync('collectedList', collectedList)
+    this.setData({ postData })
+    wx.showToast({
+      title: collected ? "收藏成功" : "取消成功",
+      duration: 1000
+    })
+  },
+  showShare: function (event) {
+    let itemList = [
+      "分享到QQ",
+      "分享到微信",
+      "分享到新浪微博"
+    ]
+    wx.showActionSheet({
+      itemList: itemList,
+      itemColor: "#b3d4db",
+      success: function (res) {
+        if (res.cancel) { return }
+        console.log('success', res)
+        wx.showToast({ title: itemList[res.tapIndex] })
+      },
+      fail: function (res) {
+        console.log('fail', res)
+        wx.showToast({
+          title: "取消"
+        })
+      }
+    })
+  },
+  handlerMusicCtr: function (e) {
+    let { postData: {music}, musicStatus } = this.data
+
+    if (musicStatus) {
+      wx.stopBackgroundAudio()
+      // this.setData({
+      //   musicStatus: false
+      // })
+    } else {
+      wx.playBackgroundAudio({
+        dataUrl: music.url,
+        title: music.title,
+        coverImgUrl: music.image
+      })
+      // this.setData({
+      //   musicStatus: true
+      // })
+    }
+
   }
 })
